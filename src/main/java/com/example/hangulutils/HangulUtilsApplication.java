41a9e269e9de8b32eec3331a3fbf5766a4ad3238
y00913 <ayt0913@gmail.com>
2024-03-01 21:19:56 +0900

    feat: init project

---
+++ src/main/java/com/example/hangulutils/HangulUtilsApplication.java
@@ -0,0 +1,13 @@
+package com.example.hangulutils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HangulUtilsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HangulUtilsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hangulutils/HangulUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hangulutils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HangulUtilsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
