a6acb0a9ca32a7803419691b8cae1e2ed3461da8
y00913-laptop <y00913@naver.com>
2024-03-24 19:57:41 +0900

    refactor: 구성 변경

--- src/main/java/com/example/LibraryTestPrint.java
+++ src/main/java/com/example/LibraryTestPrint.java
@@ -6,8 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 public class LibraryTestPrint {
 
     public String something() {
-        LibraryTest libraryTest = new LibraryTest();
-        return libraryTest.getName();
+        return "test";
     }
 
 }
