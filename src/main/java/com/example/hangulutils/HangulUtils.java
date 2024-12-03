b1678e1c1b43b84b8d871801a30a857815d17dde
yutae <y00913@naver.com>
2024-12-03 13:54:09 +0900

    refactor: 구조 리팩토링

--- src/main/java/com/example/hangulutils/HangulUtils.java
+++ src/main/java/com/example/hangulutils/HangulUtils.java
@@ -9,6 +9,20 @@ public final class HangulUtils{
     private static final List<Character> InitialList = Arrays.asList('ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ');
     private static final List<Character> doubleInitialList = Arrays.asList('ㄲ', 'ㄸ', 'ㅃ', 'ㅆ', 'ㅉ');
 
+    // 해당 초성으로 시작하는 리스트 리턴
+    public static List<String> getListByCho(Character chosung, List<String> list) {
+        List<String> dataList = new ArrayList<>();
+
+        for (String data : list) {
+            Character initial = getInitial(data);
+            if (isInitialMatch(chosung, initial)) {
+                dataList.add(data);
+            }
+        }
+
+        return dataList;
+    }
+
     // 해당 문자열의 초성 리턴
     private static Character getInitial(String data) {
         Character firstChar = data.charAt(0);
@@ -23,27 +37,13 @@ public final class HangulUtils{
     }
 
     // 해당 초성과 매치되는지 확인
-    private static Boolean isInitialMatch(Character initial, Character selectedCho) {
-        if (doubleInitialList.contains(initial)) {
+    private static Boolean isInitialMatch(Character selectedCho, Character initial) {
+        if (!doubleInitialList.contains(selectedCho) && doubleInitialList.contains(initial)) {
             int index = InitialList.indexOf(initial);
             initial = InitialList.get(index - 1);
         }
 
         return initial.equals(selectedCho);
-    }
-
-    // 해당 초성으로 시작하는 리스트 리턴
-    public static List<String> getListByCho(Character chosung, List<String> list) {
-        List<String> dataList = new ArrayList<>();
-
-        for (String data : list) {
-            Character initial = getInitial(data);
-            if (isInitialMatch(initial, chosung)) {
-                dataList.add(data);
-            }
-        }
-
-        return dataList;
     }
 
 }
