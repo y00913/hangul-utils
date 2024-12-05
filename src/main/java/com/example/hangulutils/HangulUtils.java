8220f2c5a8bc8be7a241c8d16b1768aa679916ec
idfarmdev <idfarmdev@gmail.com>
2024-12-05 16:47:38 +0900

    refactor: 구조 리팩토링

--- src/main/java/com/example/hangulutils/HangulUtils.java
+++ src/main/java/com/example/hangulutils/HangulUtils.java
@@ -8,14 +8,16 @@ public final class HangulUtils{
 
     private static final List<Character> InitialList = Arrays.asList('ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ');
     private static final List<Character> doubleInitialList = Arrays.asList('ㄲ', 'ㄸ', 'ㅃ', 'ㅆ', 'ㅉ');
+    private static final Integer koreanStartUnicode = 0xAC00;
+    private static final Integer jungsungUnicode = 0x1C;
+    private static final Integer jongsungUnicode = 0x15;
 
     // 해당 초성으로 시작하는 리스트 리턴
     public static List<String> getListByCho(Character chosung, List<String> list) {
         List<String> dataList = new ArrayList<>();
-
         for (String data : list) {
-            Character initial = getInitial(data);
-            if (isInitialMatch(chosung, initial)) {
+            Character firstInitial = getFirstInitial(data);
+            if (isEqual(chosung, firstInitial)) {
                 dataList.add(data);
             }
         }
@@ -24,26 +26,26 @@ public final class HangulUtils{
     }
 
     // 해당 문자열의 초성 리턴
-    private static Character getInitial(String data) {
+    private static Character getFirstInitial(String data) {
         Character firstChar = data.charAt(0);
-        int uniCode = (int) firstChar - 0xAC00;
-        int initialIndex = (int) Math.floor(uniCode / 588);
+        int firstCharUnicode = (int) firstChar - koreanStartUnicode;
+        int firstInitialIndex = (int) Math.floor(firstCharUnicode / (jungsungUnicode * jongsungUnicode));
 
-        if (initialIndex >= 0 && initialIndex < InitialList.size()) {
-            return InitialList.get(initialIndex);
+        if (firstInitialIndex < 0 || firstInitialIndex >= InitialList.size()) {
+            return ' ';
         }
 
-        return ' ';
+        return InitialList.get(firstInitialIndex);
     }
 
     // 해당 초성과 매치되는지 확인
-    private static Boolean isInitialMatch(Character selectedCho, Character initial) {
-        if (!doubleInitialList.contains(selectedCho) && doubleInitialList.contains(initial)) {
-            int index = InitialList.indexOf(initial);
-            initial = InitialList.get(index - 1);
+    private static Boolean isEqual(Character selectedCho, Character firstInitial) {
+        if (!doubleInitialList.contains(selectedCho) && doubleInitialList.contains(firstInitial)) {
+            int index = InitialList.indexOf(firstInitial);
+            firstInitial = InitialList.get(index - 1);
         }
 
-        return initial.equals(selectedCho);
+        return firstInitial.equals(selectedCho);
     }
 
 }
